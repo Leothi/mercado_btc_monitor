8320e3b32cd220ef96b1e27ee5c5e7db498a7aa2
Leothi <leohb2@gmail.com>
2021-01-02 11:35:50 -0300

    feat finalizada

--- mercado_btc_monitor/api/routes/messaging.py
+++ mercado_btc_monitor/api/routes/messaging.py
@@ -15,37 +15,10 @@ def router_current(disable_notifications: bool = Query(True, description="Notifi
     return {"enviado": TelegramNotifier.send_current_price(disable_notifications=disable_notifications)}
 
 
-@router.get('/if_gt_target_price', response_model=SendResponse, summary="Notificação de valor atual maior que target.")
-def router_gt_target(disable_notifications: bool = Query(False, description="Notificação silenciosa (apenas vibração).")) -> dict:
-    """Compara o valor atual com o Target e notifica via Telegram caso Atual >= Target.
-
-    Ideal para operações de venda.
-
-    Envia somente se a notificação foi ativada e o preço alvo settado.
-    """
-
-    logger.log('LOG ROTA', "Chamada rota /if_gt_target_price.")
-    return {"enviado": TelegramNotifier.send_if_gt_target_price(disable_notifications=disable_notifications)}
-
-
-@router.get('/if_lt_target_price', response_model=SendResponse, summary="Notificação de valor atual menor que target.")
-def router_lt_target(disable_notifications: bool = Query(False, description="Notificação silenciosa (apenas vibração).")) -> dict:
-    """Compara o valor atual com o Target e notifica via Telegram caso Atual <= Target.
-
-    Ideal para operações de compra.
-
-    Envia somente se a notificação foi ativada e o preço alvo settado.
-    """
-
-    logger.log('LOG ROTA', "Chamada rota /if_lt_target_price.")
-    return {"enviado": TelegramNotifier.send_if_lt_target_price(disable_notifications=disable_notifications)}
-
-@router.get('/if_target_price', response_model=SendResponse, summary="Notificação de valor atual menor que target.")
+@router.get('/if_target_price', response_model=SendResponse, summary="Notificação de valor atual menor ou maior que target.")
 def router_target(comparison_type: Comparison = Query(..., description="Qual tipo de comparação será feita entre o preço atual com o target price."),
                   disable_notifications: bool = Query(False, description="Notificação silenciosa (apenas vibração).")) -> dict:
-    """Compara o valor atual com o Target e notifica via Telegram caso Atual <= Target.
-
-    Ideal para operações de compra.
+    """Compara o valor atual com o Target e notifica via Telegram caso Atual <= Target ou Atual >= Target dependendo do tipo de comparação.
 
     Envia somente se a notificação foi ativada e o preço alvo settado.
     """
