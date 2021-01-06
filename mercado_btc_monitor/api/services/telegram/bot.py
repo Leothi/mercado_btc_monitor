06079e636140e3114332490684a99b627275768f
Leothi <leohb2@gmail.com>
2021-01-06 15:43:16 -0300

    botao current funcionando

--- mercado_btc_monitor/api/services/telegram/bot.py
+++ mercado_btc_monitor/api/services/telegram/bot.py
@@ -1,6 +1,7 @@
 from telegram import Update, Bot, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackContext, ConversationHandler, CallbackQueryHandler
 
+from api.modules.telegram_notifier import TelegramNotifier
 from api.settings import envs
 
 
@@ -8,31 +9,56 @@ class TelegramBOT:
     def __init__(self):
         self.bot = Bot(envs.BOT_TOKEN)
         self.bot_token = envs.BOT_TOKEN
+        
+    @staticmethod
+    def price(update: Update, context: CallbackContext):
+        """
+        docstring
+        """
+        keyboard = [
+            [
+                InlineKeyboardButton("Preço atual", callback_data='current'),
+                InlineKeyboardButton("Preço alvo", callback_data='target'),
+            ]
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
 
+        update.message.reply_text('Please choose:', reply_markup=reply_markup)
+        
+    @staticmethod
+    def button(update: Update, context: CallbackContext) -> None:
+        query = update.callback_query
+        query.answer()
+
+        if query.data == 'current':
+            TelegramNotifier.send_current_price(disable_notifications=False)
+            text = "Preço atual."
+        else:
+            print("pass")
+        query.edit_message_text(text=text)
+        
+        
     @staticmethod
     def start(update: Update, context: CallbackContext):
-        # context.bot.send_message(chat_id=update.effective_chat.id, text="Bot de monitoração de Bitcoin!")
-        reply_keyboard = [['Boy', 'Girl', 'Other']]
-
+        reply_keyboard = [
+            ['Boy', 'Girl', 'Other']
+        ]
+        reply_markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
         update.message.reply_text(
             'Hi! My name is Professor Bot. I will hold a conversation with you. '
             'Send /cancel to stop talking to me.\n\n'
             'Are you a boy or a girl?',
-            reply_markup=ReplyKeyboardMarkup(
-                reply_keyboard, one_time_keyboard=True),)
+            reply_markup=reply_markup)
 
     @staticmethod
     def keyboard(update: Update, context: CallbackContext):
         query = update.callback_query
-        # Get Bot from CallbackContext
         bot = context.bot
 
         keyboard = [
-            [InlineKeyboardButton("Preço Atual", callback_data='teste'),
-             InlineKeyboardButton("Atual > Target", callback_data='HRlist8'),
-             InlineKeyboardButton("Atual < Target", callback_data='HRlist8')],
+            ["Preço Atual", "Atual > Target", "Atual < Target",]
         ]
-        reply_markup = InlineKeyboardMarkup(keyboard)
+        reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
         update.message.reply_text('Selecione o tipo de operação:',
                                   reply_markup=reply_markup,)
 
@@ -42,9 +68,13 @@ class TelegramBOT:
 
         start_handler = CommandHandler('start', self.start)
         keyboard_handler = CommandHandler('keyboard', self.keyboard)
+        price_handler = CommandHandler('price', self.price)
+        button_handler = CallbackQueryHandler(self.button)
 
         dispatcher.add_handler(start_handler)
+        dispatcher.add_handler(price_handler)
         dispatcher.add_handler(keyboard_handler)
+        dispatcher.add_handler(button_handler)
 
         updater.start_polling()
         updater.idle()
